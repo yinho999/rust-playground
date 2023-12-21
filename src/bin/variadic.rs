c8ad3173a5c608035933e2be1c49163373d67fbf
Naiker <yinho999@gmail.com>
2023-12-21 23:14:39 +0000

    Fixed a bit of documentation

--- src/bin/variadic.rs
+++ src/bin/variadic.rs
@@ -4,6 +4,24 @@ pub trait MyFunction<T>: Send + Sync + 'static {
 }
 
 // Step 2: Define the macro
+///
+/// For example: For 3 arguments which is `$name!([T1, T2, T3])` in [`all_the_tuples!`], the [`impl_my_function!`] macro will generate the following code:
+/// ``` rust
+/// impl<F, T1, T2, T3> MyFunction<(T1, T2, T3)> for F
+///    where
+///         F: Fn(T1, T2, T3) + Send + Sync + Clone + 'static,
+///         T1: Send,
+///         T2: Send,
+///         T3: Send,
+/// {
+///    fn call(&self, args: (T1, T2, T3)) {
+///       let (t1, t2, t3) = args;
+///      (self.clone())(t1, t2, t3);
+///   }
+/// }
+/// ```
+///
+
 macro_rules! impl_my_function {
     ([$($ty:ident),*]) => {
         impl<F, $($ty,)*> MyFunction<($($ty,)*)> for F
@@ -18,7 +36,25 @@ macro_rules! impl_my_function {
         }
     };
 }
-// Step 3: all_the_tuples!() macro is to generate the 0-3 arguments with any type for the called macro, in this case impl_my_function!
+/// The purpose is: [`all_the_tuples`] macro is used to generate implementations of the [`MyFunction`] trait for functions with different numbers of parameters. It does this by invoking the [`impl_my_function`] macro with different numbers of generic type parameters.
+///
+/// In Short: [`all_the_tuples!()`] macro is to generate the 0-3 arguments with any type for the called macro, in this case [`impl_my_function!`] macro.
+///
+/// For example
+/// ``` rust
+/// all_the_tuples!(impl_my_function)
+/// ```
+/// will generate the following code:
+/// ``` rust
+/// macro_rules! all_the_tuples {
+///    (impl_my_function) => {
+///          impl_my_function!([]);
+///         impl_my_function!([T1]);
+///         impl_my_function!([T1, T2]);
+///         impl_my_function!([T1, T2, T3]);
+///     };
+///  }
+/// ```
 macro_rules! all_the_tuples {
     ($name:ident) => {
         $name!([]);
